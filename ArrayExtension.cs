4125eb761dd2af75659f80ff33fb2fd462b3f6e0
Sertunc SELEN <sertuncselen@yahoo.com>
2018-05-16 14:48:55 +0300

    Create ArrayExtension.cs

---
+++ ArrayExtension.cs
@@ -0,0 +1,28 @@
+public static class ArrayExtension
+{
+    /// <summary>
+    /// Boşluk olacak şekilde join işlemini gerçekleştirir.
+    /// </summary>
+    /// <param name="value"></param>
+    /// <returns></returns>
+    public static String JoinSpace(this String[] value)
+        => string.Join(" ", value);
+    
+    /// <summary>
+    /// Dizileriniz için tek satırda foreach ve işlemi gerçekleştirebilirsiniz.
+    /// </summary>
+    /// <typeparam name="T">Dizi türü.</typeparam>
+    /// <param name="values">Dizi nesnenin.</param>
+    /// <param name="action">Her bir dizi elemanınızda yapacağınız işlem.</param>
+    /// <example>
+    /// public void AddFields(params Type1[] objects)
+    /// {
+    ///     objects.ForEach(o => _objects.Add(o.ToLower()));
+    /// }
+    /// </example>
+    public static void ForEach<T>(this T[] values, Action<T> action)
+    {
+        foreach (var value in values)
+            action(value);
+    }
+}
